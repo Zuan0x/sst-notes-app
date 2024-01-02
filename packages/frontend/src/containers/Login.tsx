88fb97f84cb7a1c522ccc6032a99de80b5e2c028
Zuan0x <zuan.0x@gmail.com>
2024-01-02 16:18:07 +0000

    Add LoaderButton

--- packages/frontend/src/containers/Login.tsx
+++ packages/frontend/src/containers/Login.tsx
@@ -1,66 +1,71 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Stack from "react-bootstrap/Stack";
-import Button from "react-bootstrap/Button";
-import "./Login.css";
-import { Auth } from "aws-amplify";
-import { useAppContext } from "../lib/contextLib";
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Stack from 'react-bootstrap/Stack';
+import './Login.css';
+import LoaderButton from '../components/LoaderButton';
+import { Auth } from 'aws-amplify';
+import { useAppContext } from '../lib/contextLib';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
 
-const { userHasAuthenticated } = useAppContext();
-  
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
+	const { userHasAuthenticated } = useAppContext();
 
-async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+	const nav = useNavigate();
 
-    try {
-        await Auth.signIn(email, password);
-       userHasAuthenticated(true);
-    } catch (error) {
-        // Prints the full error
-        console.error(error);
-        if (error instanceof Error) {
-            alert(error.message);
-        } else {
-            alert(String(error));
-        }
-    }
-}
+	function validateForm() {
+		return email.length > 0 && password.length > 0;
+	}
 
-  return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Stack gap={3}>
-          <Form.Group controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              autoFocus
-              size="lg"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              size="lg"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Button size="lg" type="submit" disabled={!validateForm()}>
-            Login
-          </Button>
-        </Stack>
-      </Form>
-    </div>
-  );
+	async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+
+		setIsLoading(true);
+
+		try {
+			await Auth.signIn(email, password);
+			userHasAuthenticated(true);
+			nav('/');
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			} else {
+				alert(String(error));
+			}
+			setIsLoading(false);
+		}
+	}
+	return (
+		<div className="Login">
+			<Form onSubmit={handleSubmit}>
+				<Stack gap={3}>
+					<Form.Group controlId="email">
+						<Form.Label>Email</Form.Label>
+						<Form.Control
+							autoFocus
+							size="lg"
+							type="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</Form.Group>
+					<Form.Group controlId="password">
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							size="lg"
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</Form.Group>
+					<LoaderButton size="lg" type="submit" isLoading={isLoading} disabled={!validateForm()}>
+						Login
+					</LoaderButton>{' '}
+				</Stack>
+			</Form>
+		</div>
+	);
 }
